c547ec87748f819fbdd6ab96510c40ece8577de8
Rakari <Bollagardar@Stori-iMac.local>
2015-04-10 23:27:43 +0000

    Hax
    Haxað

--- R/sumZero.R
+++ R/sumZero.R
@@ -1,3 +1,13 @@
+#' Sum of Vector Elements
+#' 
+#' Takes in a numeric vector and returns the sum of it. If at least one entry is numeric, then the function returns 0. If no entry is a value, then it returns NA.
+#' @param x A numeric vector to find the sum of
+#' @return The sum of the vector x
+#' @examples 
+#' x <- c(NA,2)
+#' sumZero(x) 
+#' returns: 0
+#' @export
 sumZero <- function(x) {
      if(!(class(x) %in% c("numeric", "logical"))) {
           stop("The input value is not able to convert to a numeric vector")
